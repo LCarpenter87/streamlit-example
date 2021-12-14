ace9394416ed159096e0e95563fb0c3f87183dbd
LCarpenter87 <L.carpenter@outlook.com>
2021-12-14 14:10:40 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,7 @@ st.title('Testing')
 
 sentence1 = st.text_input('Input your sentence here:') 
 sentence2 = st.text_input('Input your second sentence here:') 
+carmodel = st.text_input('Please give the car model:') 
 
 example = st.slider('Input Example' , min_value=0, max_value=1000, value=500, step=10)
 
@@ -16,6 +17,6 @@ kindwords = st.radio('How often did you say Please and Thank you?', options, ind
 
 
 if st.button("Let's go", key=None, help=None, on_click=None, args=None, kwargs=None):
-  st.text(f'{sentence1} and {sentence2} and {example}')
+  st.text(f'{sentence1} and {sentence2} and {example} and {carmodel}')
   
 
