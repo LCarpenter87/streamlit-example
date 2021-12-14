7331cb6ead96002f145752c6ba475daa6524200f
LCarpenter87 <L.carpenter@outlook.com>
2021-12-14 12:57:07 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,9 @@ sentence2 = st.text_input('Input your second sentence here:')
 
 example = st.slider('Input Example' , min_value=0, max_value=1000, value=500, step=10)
 
+options = ['Every Time', 'Most of the Time', 'Rarely', 'Never']
+
+kindwords = st.selectbox('How often did you say Please and Thank you?', options, index=0, format_func=special_internal_function, key=None, help=None, on_change=None, args=None, kwargs=None)
 
 
 if st.button("Let's go", key=None, help=None, on_click=None, args=None, kwargs=None):
