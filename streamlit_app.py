5cc0d29462bb9c9526826e4a427218af9949c2a0
LCarpenter87 <L.carpenter@outlook.com>
2021-12-16 13:26:12 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import numpy as np
 
 
 question = st.selectbox("What question do you want?", ["How tall is this?", "how wide is this?"], index=0)
-answer = st.text_area("What's the answer") , value="")
+answer = st.text_area("What's the answer" , value="")
 
 if st.button("Let's go", key=None, help=None, on_click=None, args=None, kwargs=None):
   st.text(f'Your question was {question}, your answer was {answer}')
