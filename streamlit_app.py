f0e9cbbadfa95af34b1a1a5bcf3c61e029fa156d
LCarpenter87 <L.carpenter@outlook.com>
2022-01-28 22:19:43 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,8 @@ def generate_reg(letters, orange, include, exclude):
 st.header("Let's Wordle!")
 
 
-st.header("Green Letters")
+st.title("Green Letters")
+st.caption("Put in your green known letters, in the correct position")
 letters = {}
 l1, l2, l3, l4, l5, space = st.columns([1,1,1,1,1, 5])
 letters[1] = l1.text_input('1st letter', value="", max_chars=1)
@@ -46,10 +47,12 @@ letters[4] = l4.text_input('4th letter', value="", max_chars=1)
 letters[5] = l5.text_input('5th letter', value="", max_chars=1)
 
 
-st.header("Gray Letters")
+st.title("Gray Letters")
+st.caption("Put in your gray letters, in any order (no spaces)")
 exclude = st.text_input('Gray Letters', value="")
 orange = {}
 st.header("Orange Letters")
+st.caption("Put in your orange letters, in the position you tried them in")
 
 o1, o2, o3, o4, o5 = st.columns([1,1,1,1,1])
 orange[1] = o1.text_input('1st letter', value="", max_chars=5)
@@ -65,7 +68,7 @@ for v in orange.values():
 
 if st.button("Word me up baby", key=None, help=None, on_click=None, args=None, kwargs=None):
   reg = generate_reg(letters, orange, include, exclude)
-  st.text(reg)
+  st.text(f'The Regex used was: {reg}')
   create_possible()
   answers = find_poss(possible, reg)
   st.text(f'Found {len(answers)} words')
