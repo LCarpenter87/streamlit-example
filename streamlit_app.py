614c89b7100dad4b1345619fc0f65d88b7ab1f55
LCarpenter87 <L.carpenter@outlook.com>
2022-01-28 22:03:41 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,11 +38,11 @@ st.header("Let's Wordle!")
 
 st.header("Green Letters")
 letters = {}
-letters[1] = st.text_area('1st letter', value="", height=None, max_chars=1, key=None, help=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False)
-letters[2] = st.text_area('2nd letter', value="", height=None, max_chars=1, key=None, help=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False)
-letters[3] = st.text_area('3rd letter', value="", height=None, max_chars=1, key=None, help=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False)
-letters[4] = st.text_area('4th letter', value="", height=None, max_chars=1, key=None, help=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False)
-letters[5] = st.text_area('5th letter', value="", height=None, max_chars=1, key=None, help=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False)
+letters[1] = st.text_area('1st letter', value="", height=1, max_chars=1, key=None, help=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False)
+letters[2] = st.text_area('2nd letter', value="", height=2, max_chars=1, key=None, help=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False)
+letters[3] = st.text_area('3rd letter', value="", height=3, max_chars=1, key=None, help=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False)
+letters[4] = st.text_area('4th letter', value="", height=4, max_chars=1, key=None, help=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False)
+letters[5] = st.text_area('5th letter', value="", height=5, max_chars=1, key=None, help=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False)
 
 
 st.header("Gray Letters")
@@ -51,8 +51,8 @@ exclude = st.text_area('Gray Letters', value="", height=None, max_chars=None, ke
 
 orange = {}
 st.header("Orange Letters")
-orange[1] = st.text_area('1st letter', value="", height=None, max_chars=5, key=None, help=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False)
-orange[2] = st.text_area('2nd letter', value="", height=None, max_chars=5, key=None, help=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False)
+orange[1] = st.text_area('1st letter', value="", height=10, max_chars=5, key=None, help=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False)
+orange[2] = st.text_area('2nd letter', value="", height=20, max_chars=5, key=None, help=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False)
 orange[3] = st.text_area('3rd letter', value="", height=None, max_chars=5, key=None, help=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False)
 orange[4] = st.text_area('4th letter', value="", height=None, max_chars=5, key=None, help=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False)
 orange[5] = st.text_area('5th letter', value="", height=None, max_chars=5, key=None, help=None, on_change=None, args=None, kwargs=None, placeholder=None, disabled=False)
