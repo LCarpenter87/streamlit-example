28e58d7d8d6a5e99c0944e682481ee0cea954aa0
LCarpenter87 <L.carpenter@outlook.com>
2022-03-21 14:52:11 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,4 @@ st.header("DS4 > DS3 == True")
 number1 = st.text_input("First number?")
 number2 = st.text_input("Second number")
 
-st.text(int(number1) + int(number2)) 
+st.title(int(number1) + int(number2)) 
